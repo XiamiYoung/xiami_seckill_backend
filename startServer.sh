7e8a2b99e5b86b8114daf2d9ec610a3b25139b1e
Young Xue <young.xue@sap.com>
2021-09-28 22:56:48 +0800

    update context

--- startServer.sh
+++ startServer.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -l
-nohup /usr/local/bin/uvicorn xiami_seckill_backend.asgi:websocket_application --host="0.0.0.0" --port=8000 --limit-concurrency 100 --reload &
+nohup /usr/local/bin/uvicorn xiami_seckill_backend.asgi:websocket_application --host="0.0.0.0" --port=7366 --limit-concurrency 100 --reload &
